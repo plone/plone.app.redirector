a91bb3cb407990512b65e5f78491512a002406f6
Andreas Zeidler <witsch@plone.org>
2008-07-06 16:40:38 +0000

    add another test to check for creation of redirection entries for new object — now ttw, and this time it does indeed fail... (see #8260)
    svn path=/plone.app.redirector/trunk/; revision=21392

--- plone/app/redirector/tests/base.py
+++ plone/app/redirector/tests/base.py
@@ -1,3 +1,4 @@
+from Products.Five.testbrowser import Browser
 from Products.PloneTestCase import PloneTestCase
 PloneTestCase.setupPloneSite()
 
@@ -5,4 +6,13 @@ class RedirectorTestCase(PloneTestCase.PloneTestCase):
     pass
     
 class RedirectorFunctionalTestCase(PloneTestCase.FunctionalTestCase):
-    pass
+
+    def getBrowser(self, loggedIn=True):
+        """ instantiate and return a testbrowser for convenience """
+        browser = Browser()
+        if loggedIn:
+            user = PloneTestCase.default_user
+            pwd = PloneTestCase.default_password
+            browser.addHeader('Authorization', 'Basic %s:%s' % (user, pwd))
+        return browser
+
