5e195cebc4a19c8e0daf2f5250150fd74d7d4d93
Maurits van Rees <maurits@vanrees.org>
2015-08-14 16:50:27 +0200

    Added classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ version = '1.2.2.dev0'
 setup(name='plone.app.redirector',
       version=version,
       description="redirection tool",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
+      long_description=(open("README.rst").read() + "\n" +
+                        open("CHANGES.rst").read()),
       classifiers=[
           "Environment :: Web Environment",
           "Framework :: Plone",
+          "Framework :: Plone :: 5.0",
           "Framework :: Zope2",
           "License :: OSI Approved :: GNU General Public License (GPL)",
           "Operating System :: OS Independent",
           "Programming Language :: Python",
-        ],
+          "Programming Language :: Python :: 2.7",
+      ],
       keywords='',
       author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
@@ -25,8 +27,8 @@ setup(name='plone.app.redirector',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
-        'plone.memoize',
+          'setuptools',
+          'plone.memoize',
       ],
       extras_require={
           'test': [
