8a75ae06550c0fb9fa88b897f269007e9b427831
Maurits van Rees <maurits@vanrees.org>
2019-02-13 22:10:23 +0100

    Use timeit context manager to report on the performance.

--- plone/app/redirector/tests/test_performance.py
+++ plone/app/redirector/tests/test_performance.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from contextlib import contextmanager
 from plone.app.redirector.storage import RedirectionStorage
 from time import time
 
@@ -14,6 +15,27 @@ class TestStoragePerformance(unittest.TestCase):
     """Test the performance of the RedirectionStorage class.
     """
 
+    @contextmanager
+    def timeit(self, message, limit=0):
+        start = time()
+        yield
+        end = time()
+        total = end - start
+        # Allow taking at least 0.1 seconds.  Otherwise a really low NUMBER
+        # like 0 may give errors like this:
+        # AssertionError: Listing all takes too long: 0.00 seconds (max 1e-06)
+        limit = max(limit, 0.1)
+        if total > limit:
+            self.fail(
+                '{0} takes too long: {1:.2f} seconds (max {2})'.format(
+                    message, total, limit
+                )
+            )
+        else:
+            print(
+                '{0}: {1:.2f} seconds (max {2})'.format(message, total, limit)
+            )
+
     @unittest.skipIf(NUMBER <= 0, '{0} env variable not set'.format(env_name))
     def test_storage_performance(self):
         """Test the performance of some of the code.
@@ -24,59 +46,47 @@ class TestStoragePerformance(unittest.TestCase):
 
             $ bin/test -s plone.app.redirector -m test_performance -vvv
 
-        Sample run with one hundred thousand inserts:
+        Sample run with one million inserts:
 
-            $ PLONE_APP_REDIRECTOR_PERFORMANCE_NUMBER=100000 \
+            $ PLONE_APP_REDIRECTOR_PERFORMANCE_NUMBER=1000000 \
                 bin/test -s plone.app.redirector -m test_performance
             ...
             Running plone.app.redirector storage performance tests.
-            Inserting 100000 paths...
-            Inserting: 0.74 seconds
-            Getting length: 0.00 seconds
-            Getting iterator: 0.00 seconds
-            Listing all: 0.01 seconds
-            Listing and getting each single one: 0.16 seconds
+            Inserting 1000000 paths...
+            Inserting: 9.39 seconds (max 1000.0)
+            Getting length: 0.01 seconds (max 0.1)
+            Getting iterator: 0.00 seconds (max 0.1)
+            Listing all: 0.05 seconds (max 1.0)
+            Listing and getting each single one: 1.82 seconds (max 10.0)
 
         """
         st = RedirectionStorage()
 
-        start_time = time()
         print('\nRunning plone.app.redirector storage performance tests.')
         print('Inserting {0} paths...'.format(NUMBER))
-        for i in range(NUMBER):
-            st['/old/{0}'.format(i)] = '/new/{0}'.format(i)
-        insert_time = time()
-        insert_seconds = insert_time - start_time
-        print('Inserting: {0:.2f} seconds'.format(insert_seconds))
+        # Can take long.  But 10.000 per second should be no problem.
+        with self.timeit('Inserting', NUMBER / 10000.0):
+            for i in range(NUMBER):
+                st['/old/{0}'.format(i)] = '/new/{0}'.format(i)
 
-        # len
-        self.assertEqual(len(st), NUMBER)
-        length_time = time()
-        length_seconds = length_time - insert_time
-        print('Getting length: {0:.2f} seconds'.format(length_seconds))
+        # Should be almost instantaneous.
+        with self.timeit('Getting length'):
+            self.assertEqual(len(st), NUMBER)
 
-        # iter
-        iter(st)
-        iter_time = time()
-        iter_seconds = iter_time - length_time
-        print('Getting iterator: {0:.2f} seconds'.format(iter_seconds))
+        # Should be almost instantaneous.
+        with self.timeit('Getting iterator'):
+            iter(st)
 
-        # list
-        list(st)
-        list_time = time()
-        list_seconds = list_time - iter_time
-        print('Listing all: {0:.2f} seconds'.format(list_seconds))
+        # Should be fairly quick.
+        with self.timeit('Listing all', NUMBER / 1000000.0):
+            list(st)
 
-        # list + get
-        for key in st:
-            st[key]
-        getall_time = time()
-        getall_seconds = getall_time - list_time
-        print(
-            'Listing and getting each single one: {0:.2f} seconds'.format(
-                getall_seconds
-            )
-        )
+        # Should be reasonably quick, but the time is noticeable.
+        with self.timeit(
+            'Listing and getting each single one', NUMBER / 100000.0
+        ):
+            for key in st:
+                st[key]
 
 
 def test_suite():
