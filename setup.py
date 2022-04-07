4c2b760f23923c9c388321fd9d35155a023ad306
Jens W. Klein <jk@kleinundpartner.at>
2022-04-07 17:48:12 +0200

    prepare plone 6 only

--- setup.py
+++ setup.py
@@ -1,49 +1,40 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
-version = '2.2.2.dev0'
 
-setup(name='plone.app.redirector',
-      version=version,
-      description="redirection tool",
-      long_description=(open("README.rst").read() + "\n" +
-                        open("CHANGES.rst").read()),
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Plone :: 5.0",
-          "Framework :: Plone :: 5.1",
-          "Framework :: Plone :: 5.2",
-          "Framework :: Plone :: Core",
-          "Framework :: Zope2",
-          "Framework :: Zope :: 4",
-          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-      ],
-      keywords='links redirect',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://pypi.org/project/plone.app.redirector',
-      license='GPL version 2',
-      packages=find_packages(),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.memoize',
-          'six',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'plone.app.contenttypes'
-          ]
-      },
-      )
+version = "3.0.0.dev0"
+
+setup(
+    name="plone.app.redirector",
+    version=version,
+    description="redirection tool",
+    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="links redirect",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.app.redirector",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "plone.memoize",
+    ],
+    extras_require={"test": ["plone.app.testing", "plone.app.contenttypes"]},
+)
